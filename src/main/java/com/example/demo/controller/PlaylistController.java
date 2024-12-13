aee38c339098b52ce746f900aedf27ed1932815a
Ayesha Jagirdaar <103125439+Ayesha506@users.noreply.github.com>
2024-12-13 20:11:54 +0530

    Tune hub project
    Tune hub is an online music streaming platform  where  users  canlisten music by getting subscription

---
+++ src/main/java/com/example/demo/controller/PlaylistController.java
@@ -0,0 +1,41 @@
+package com.example.demo.controller;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import com.example.demo.entities.PlayList;
+import com.example.demo.entities.Song;
+import com.example.demo.services.PlaylistService;
+import com.example.demo.services.SongService;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class PlaylistController {
+	@Autowired
+	PlaylistService playlistService;
+	@Autowired
+	SongService songService;
+	@GetMapping("/createPlaylist")
+	public String createPlaylist(Model model) {
+		List<Song> songList=songService.fetchAllSongs();
+		model.addAttribute("songs", songList);
+		return "createPlaylist";
+	}
+	@PostMapping("/addPlaylist")
+	public String addPlaylist(@ModelAttribute PlayList playlist) {
+		playlistService.addPlaylist(playlist);
+		List<Song> songList = playlist.getSongs();
+		for(Song s: songList) {
+			s.getPlaylists().add(playlist);
+			songService.updateSong(s);
+		}
+		return "adminHome";
+	}
+	@GetMapping("/viewPlaylists")
+	public String viewPlaylists(Model model) {
+		List<PlayList> allPlaylists = playlistService.fetchAllPlaylists();
+		model.addAttribute("allPlaylists", allPlaylists);
+		return "displayPlaylists"; //dynamic page
+	}
+}
---
+++ src/main/java/com/example/demo/controller/UsersController.java
@@ -0,0 +1,62 @@
+package com.example.demo.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entities.LoginData;
+import com.example.demo.entities.Users;
+import com.example.demo.services.UsersService;
+import jakarta.servlet.http.HttpSession;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+@CrossOrigin("*")
+@RestController
+public class UsersController {
+	@Autowired
+	UsersService service;
+	@PostMapping("/register")
+	public String addUsers(@ModelAttribute Users user) {
+		System.out.println("addusers method is called");
+		boolean userStatus = service.emailExists(user.getEmail());
+		if(userStatus==false) {
+		service.addUser(user);
+		System.out.println("user added");
+		}
+		else {
+			System.out.println("user already exists");
+		}
+		System.out.println("addusers method is completed");
+		return "home";
+	}
+	@PostMapping("/validate")
+	public String validate(@RequestBody LoginData data,HttpSession session, Model model ) {
+		String email= data.getEmail();
+		String password = data.getPassword();
+		System.out.println("call received");
+		if(service.validateUser(email, password)== true) {
+			String role =service.getRole(email);
+			if(role.equals("admin")) {
+				return "adminHome";
+			}
+			else {
+			return "customerHome";
+			}
+		}
+		else {
+			return "login";
+		}
+	}
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		session.invalidate();
+		return "login";
+	}
+	
+	
+}
